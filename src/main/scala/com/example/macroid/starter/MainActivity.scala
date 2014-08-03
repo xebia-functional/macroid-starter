2c4ea9b86fc5f61a08b41d1ffdaa15d8fc2e2fe8
madhukar <phatak.dev@gmail.com>
2014-08-03 23:50:05 +0530

    Add an example of using resources

--- src/main/scala/com/example/macroid/starter/MainActivity.scala
+++ src/main/scala/com/example/macroid/starter/MainActivity.scala
@@ -10,7 +10,6 @@ import android.app.Activity
 import macroid._
 import macroid.FullDsl._
 import macroid.contrib._
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
 // define our helpers in a mixable trait
@@ -51,6 +50,8 @@ class MainActivity extends Activity with Styles with Contexts[Activity] {
           // tweaks coming after them will wait till they finish
           cap <~ text("Button clicked!") <~~ delay(1000) <~ text("Howdy")
         }
+        //Rounded edge button using background image
+        <~ BgTweaks.res(R.drawable.glossy)
     ) <~
       // match layout orientation to screen orientation
       (portrait ? vertical | horizontal) <~ Transformer {
